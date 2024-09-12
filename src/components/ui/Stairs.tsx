05c0b048a986bc942bb58d2a7eef9a87fa0c0b09
ruiner28 <66207175+ruiner28@users.noreply.github.com>
2024-09-12 14:54:07 -0700

    Update Stairs.tsx
    added type for index

--- src/components/ui/Stairs.tsx
+++ src/components/ui/Stairs.tsx
@@ -13,7 +13,7 @@ const stairAnimation = {
     },
 };
 
-const reverseIndex = (index) => {
+const reverseIndex = (index:number) => {
     const totalSteps = 6;  //number of steps
     return totalSteps - index - 2;
 }
