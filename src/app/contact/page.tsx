4479586fb042ddf6e45eb9c3aef8efaf264f0506
ruiner28 <gpawar6444@gmail.com>
2025-04-15 17:51:10 -0700

    typo mistake corrected

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -46,7 +46,6 @@ const Contact = () => {
           <Button
             variant="outline"
             size="lg"
-            s
             className="uppercase flex items-center gap-2"
           >
             <span>Say Hello!</span>
